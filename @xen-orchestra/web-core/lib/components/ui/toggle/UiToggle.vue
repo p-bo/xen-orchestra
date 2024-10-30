07ce26513f7b9294cb9baf3b03724e02e7219072
joris K <joris.kosacki@vates.tech>
2024-10-30 11:03:56 +0100

    feat(web-core): fix issue with UiSpinner in the UiToggle component

--- @xen-orchestra/web-core/lib/components/ui/toggle/UiToggle.vue
+++ @xen-orchestra/web-core/lib/components/ui/toggle/UiToggle.vue
@@ -6,8 +6,8 @@
     </div>
     <label class="form-toggle" v-bind="wrapperAttrs">
       <input v-model="value" :disabled="isDisabled" type="checkbox" class="ui-toggle" v-bind="attrs" />
-      <span class="fake-checkbox">
-        <VtsIcon :busy="busy" accent="success" :fixed-width="false" :icon class="icon" />
+      <span class="fake-checkbox" :class="{ busy }">
+        <VtsIcon :busy accent="success" :fixed-width="false" :icon class="icon" />
       </span>
     </label>
   </div>
@@ -57,68 +57,83 @@ const icon = computed(() => {
     display: flex;
 
     .fake-checkbox {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      height: 2rem;
       width: 4rem;
       background-color: white;
-    }
-
-    .icon {
-      transition: transform 0.125s ease-in-out;
-      transform: translateX(-0.6em);
-    }
-
-    .ui-toggle:checked + .fake-checkbox > .icon {
-      transform: translateX(0.6em);
-    }
-  }
-
-  .ui-toggle {
-    font-size: inherit;
-    position: absolute;
-    pointer-events: none;
-    opacity: 0;
-  }
-
-  .icon {
-    font-size: 1.7rem;
-    position: absolute;
-    color: var(--color-neutral-background-primary);
-    border: 0.1rem solid var(--color-neutral-txt-secondary);
-    border-radius: 9rem;
-  }
-
-  .fake-checkbox {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: 2rem;
-    transition:
-      background-color 0.125s ease-in-out,
-      border-color 0.125s ease-in-out;
-    border: 0.1rem solid var(--color-neutral-txt-secondary);
-    border-radius: 9rem;
-    box-shadow: var(--shadow-100);
-  }
-
-  .ui-toggle:disabled {
-    & + .fake-checkbox {
-      border-color: var(--color-neutral-border);
-      background-color: var(--color-neutral-background-disabled);
+      transition:
+        background-color 0.125s ease-in-out,
+        border-color 0.125s ease-in-out;
+      border: 0.1rem solid var(--color-neutral-txt-secondary);
+      border-radius: 9rem;
 
       .icon {
-        border-color: var(--color-neutral-border);
+        font-size: 1.7rem;
+        position: absolute;
         color: var(--color-neutral-background-primary);
+        border: 0.1rem solid var(--color-neutral-txt-secondary);
+        border-radius: 9rem;
+        transition: transform 0.125s ease-in-out;
+        transform: translateX(-1.02rem);
+      }
+
+      &.busy {
+        border-color: var(--color-neutral-border);
+        background-color: var(--color-neutral-background-disabled);
+
+        .icon {
+          color: var(--color-normal-item-base);
+          border: 0.1rem solid var(--color-neutral-border);
+          background-color: var(--color-neutral-background-primary);
+          font-size: 1.4rem;
+          transform: translateX(-1.05rem);
+        }
       }
     }
 
-    &:checked + .fake-checkbox {
-      background-color: var(--color-success-item-disabled);
-    }
-  }
+    .ui-toggle {
+      font-size: inherit;
+      position: absolute;
+      pointer-events: none;
+      opacity: 0;
 
-  .ui-toggle:not(:disabled) {
-    &:checked + .fake-checkbox {
-      border-color: var(--color-neutral-txt-secondary);
-      background-color: var(--color-success-item-base);
+      &:checked + .fake-checkbox > .icon {
+        transform: translateX(1.02rem);
+      }
+
+      &:disabled {
+        & + .fake-checkbox {
+          border-color: var(--color-neutral-border);
+          background-color: var(--color-neutral-background-disabled);
+
+          .icon {
+            border-color: var(--color-neutral-border);
+            color: var(--color-neutral-background-primary);
+          }
+        }
+
+        &:checked + .fake-checkbox {
+          background-color: var(--color-success-item-disabled);
+        }
+      }
+
+      &:not(:disabled) {
+        &:checked + .fake-checkbox {
+          border-color: var(--color-neutral-txt-secondary);
+          background-color: var(--color-success-item-base);
+
+          &.busy {
+            border-color: var(--color-neutral-border);
+            background-color: var(--color-success-item-disabled);
+
+            .icon {
+              transform: translateX(1.05rem);
+            }
+          }
+        }
+      }
     }
   }
 }
