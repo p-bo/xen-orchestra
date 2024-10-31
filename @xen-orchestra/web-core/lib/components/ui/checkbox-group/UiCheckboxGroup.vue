29665457b0c5791eb4defbbf4353fcae398f4325
joris K <joris.kosacki@vates.tech>
2024-10-31 16:36:53 +0100

    feat(web-core) add UiCheckboxGroup component

---
+++ @xen-orchestra/web-core/lib/components/ui/checkbox-group/UiCheckboxGroup.vue
@@ -0,0 +1,53 @@
+<!-- v1 -->
+<template>
+  <div class="ui-checkbox-group-wrapper">
+    <VtsLabel v-if="slots.default" :accent>
+      <slot />
+    </VtsLabel>
+    <div class="ui-checkbox-group" :class="{ vertical }">
+      <UiCheckbox v-for="(label, index) in labels" :key="index" :accent>
+        {{ label }}
+      </UiCheckbox>
+    </div>
+    <VtsInfo v-if="slots.info" :accent>
+      <slot name="info" />
+    </VtsInfo>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VtsInfo from '@core/components/info/VtsInfo.vue'
+import VtsLabel from '@core/components/input/VtsLabel.vue'
+import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
+
+type CheckboxGroupAccent = 'brand' | 'warning' | 'danger'
+
+defineProps<{
+  vertical?: boolean
+  accent: CheckboxGroupAccent
+  labels: string[]
+}>()
+
+const slots = defineSlots<{
+  default?(): any
+  info?(): any
+}>()
+</script>
+
+<style scoped lang="postcss">
+.ui-checkbox-group-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
+  .ui-checkbox-group {
+    display: flex;
+    gap: 6.4rem;
+
+    &.vertical {
+      flex-direction: column;
+      gap: 0.8rem;
+    }
+  }
+}
+</style>
